684d68f65c38ecd325a7a0a7d1eed37200864b11
Rexford Seth Ashley <ashleyrexford47@gmail.com>
2026-08-11 01:31:45 +0000

    chore: create initial solution structure

---
+++ src/CareerAdvisor.Core/Class1.cs
@@ -0,0 +1,6 @@
+﻿namespace CareerAdvisor.Core;
+
+public class Class1
+{
+
+}
---
+++ tests/CareerAdvisor.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace CareerAdvisor.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
